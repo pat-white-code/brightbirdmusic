5d5e1b3550b66fab76548195c4b574aad0129e3d
pat-white-code <pat.k.white@gmail.com>
2020-02-19 16:46:25 -0600

    adds getTeachersForTwo route/controller

--- api/routes/teachers.js
+++ api/routes/teachers.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/teachers');
 
 router.get('/', controller.getTeachers);
 router.get('/all', controller.getAllTeachers);
+router.get('/two', controller.getTeachersForTwo);
 
 
 module.exports = router;
