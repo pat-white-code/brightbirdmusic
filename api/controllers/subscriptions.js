b620686946f13ae764548f4bef0bc6812a550f64
pat-white-code <pat.k.white@gmail.com>
2020-03-01 14:32:38 -0600

    adds route to post subscription, having testing issues with postman

--- api/controllers/subscriptions.js
+++ api/controllers/subscriptions.js
@@ -1,8 +1,10 @@
 const getClientSubscriptions = require('./modules/subscriptions/getClientSubscriptions');
 const getClientRequests = require('./modules/subscriptions/getClientRequests');
+const postSubscription = require('./modules/subscriptions/postSubscription');
 
 
 module.exports = {
   getClientSubscriptions,
-  getClientRequests
+  getClientRequests,
+  postSubscription
 }
--- api/routes/subsciptions.js
+++ api/routes/subsciptions.js
@@ -5,4 +5,6 @@ const controller = require('../controllers/subscriptions');
 router.get('/client/:client_id', controller.getClientSubscriptions);
 router.get('/requests/client/:clientId', controller.getClientRequests);
 
+router.post('/', controller.postSubscription);
+
 module.exports = router;
