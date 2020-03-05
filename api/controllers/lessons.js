1ea8627b18dfece76f1f3898d9c241c42cecfdb7
pat-white-code <pat.k.white@gmail.com>
2020-03-05 13:55:58 -0600

    adds new route to get all overdue attendnace from yesterday and beyond  given a teacher id.

--- api/controllers/lessons.js
+++ api/controllers/lessons.js
@@ -1,7 +1,9 @@
 const takeAttendance = require('./modules/lessons/takeAttendance');
 const bulkTakeAttendance = require('./modules/lessons/bulkTakeAttendance');
+const getUnloggedLessons = require('./modules/lessons/getUnloggedLessons');
 
 module.exports = {
   takeAttendance,
-  bulkTakeAttendance
+  bulkTakeAttendance,
+  getUnloggedLessons
 }
--- api/routes/lessons.js
+++ api/routes/lessons.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/lessons');
 
+router.get('/unlogged/:teacherId', controller.getUnloggedLessons);
+
 router.put('/:lessonId/attendance', controller.takeAttendance);
 router.put('/attendance/bulk', controller.bulkTakeAttendance);
+
 
 module.exports = router;
